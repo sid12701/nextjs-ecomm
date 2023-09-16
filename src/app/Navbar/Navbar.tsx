ca1bd58b7f9ff4ab89198ad2841a9a9743fa9a17
Siddhant Daryanani <siddhantdaryanani@gmail.com>
2023-09-16 21:25:33 +0530

    added cart icon and navbar and footer

---
+++ src/app/Navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link"
+import Image from "next/image"
+import logo from "../../assets/logo.png"
+import { redirect } from "next/navigation"
+import { getCart } from "@/lib/db/cart";
+import ShoppingCartButton from "./ShoppingCartButton";
+
+
+async function searchProducts(formData:FormData) {
+    "use server";
+    const searchQuery = formData.get("searchQuery")?.toString();
+    if(searchQuery){
+        redirect("/search?query=" + searchQuery)
+    }
+}
+
+export default async function Navbar (){
+
+    const cart = await getCart();
+
+    return (
+        <div className="bg-base-200">
+            <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2 ">
+                <div className="flex-1 ">
+                    <Link href="/" className="btn btn-ghost text-xl normal-case" >
+                        <Image src={logo} alt="devil cart logo" height={40} width={40}></Image>
+                        Devil Cart
+                    </Link>
+                </div>
+                <div className="flex-none gap-2">
+                    <form action={searchProducts}>
+                        <div className="form-control">
+                            <input className="input input-bordered w-full min-w-[100px]" name="searchQuery" placeholder="Search"/>
+                        </div>
+                    </form>
+                    <ShoppingCartButton cart={cart}/>
+                </div>
+            </div>
+        </div>
+)
+}
